e6105677b61ba90ffc713ca98a8363e4c352c83f
FatimaSughra110 <nirjiszehrazaidi>
2025-08-18 09:53:03 +0500

    Updated Navbar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Portfolio from "./components/Portfolio";
 
+
 function App() {
   return (
         <div className="bg-gray-100 dark:bg-gray-900 min-h-screen text-gray-900 dark:text-gray-100">
